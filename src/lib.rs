4ffc96bc0019083e6098452c8a49a2e36569db23
Daniyar Itegulov <wibkwibk@gmail.com>
2015-05-23 17:26:59 +0300

    Fixed BNumber, added BString implementation.
    Fixed support for negative numbers in BNumber and added one more test
    for it. BString has basic implementation of decode method, but not
    properly tested yet.

--- src/lib.rs
+++ src/lib.rs
@@ -76,10 +76,10 @@ impl Debug for BNumber {
 
 impl BElement<BNumber> for BNumber {
 
-    /// Decodes BNumber from array of bytes. 
+    /// Decodes `BNumber` from array of bytes. 
     /// 
-    /// Returnes `Ok((position of next token in bencode, parsed BNumber))` 
-    /// or `Error` if couldn't parse BNumber.
+    /// Returnes `Ok((position of last used byte in passed array, parsed BNumber))` 
+    /// or `Err` if couldn't parse BNumber correctly.
     /// # Examples
     ///
     /// BNumber must begin with 'i' char and end with 'e' char.
@@ -89,6 +89,8 @@ impl BElement<BNumber> for BNumber {
     /// use bencode::BNumber;
     /// assert_eq!((4, BNumber::new(300)),
     ///                BNumber::decode("i300e".as_bytes()).ok().expect("invalid"));
+    /// assert_eq!((5, BNumber::new(-204)),
+    ///                BNumber::decode("i-204e".as_bytes()).ok().expect("invalid"));
     /// ```
     ///
     /// If it's not, then error is generated.
@@ -98,30 +100,110 @@ impl BElement<BNumber> for BNumber {
     /// use bencode::BNumber;
     /// assert!(BNumber::decode("l300e".as_bytes()).is_err());
     /// ```
+    /// 
+    /// Also error is generated, when number is too big for `i64`.
+    /// 
+    /// ```
+    /// use bencode::BElement;
+    /// use bencode::BNumber;
+    /// assert!(BNumber::decode("i400000000000000000000000000000000000000000000e".as_bytes()).is_err());
+    /// ```
     fn decode(encoded: &[u8]) -> Result<(usize, BNumber), &'static str> {
         match encoded[0] as char {
             'i' => {
                 let mut i : usize = 1;
-                while i < encoded.len() && (encoded[i] as char).is_digit(10) {
+                while i < encoded.len() && encoded[i] as char != 'e' {
                     i += 1;
                 }
                 if i == encoded.len() {
                     return Err("expected 'e' after number");
                 }
                 let number: &[u8] = &encoded[1..i];
-                match encoded[i] as char {
-                    'e' => {
-                        let str_number: String = String::from_utf8_lossy(number).into_owned();
-                        if let Ok(x) = str_number.parse::<i64>() {
-                            Ok((i, BNumber::new(x)))
-                        } else {
-                            Err("expected correct i64 number")
-                        }
-                    },
-                    _ => Err("expected 'e' after number")
+                let str_number: String = String::from_utf8_lossy(number).into_owned();
+                if let Ok(x) = str_number.parse::<i64>() {
+                    Ok((i, BNumber::new(x)))
+                } else {
+                    Err("expected correct i64 number")
                 }
             },
             _ => Err("expected 'i' before number")
+        }
+    }
+}
+
+/// Struct for representing string (byte sequence) in Bencode format.
+pub struct BString {
+    /// Sequence of bytes, contained in this `BString`.
+    data: String,
+}
+
+/// Basic equivalence relation.
+///
+/// Checks for equality simply using `BString`'s `data` field. Works exactly
+/// like equivalence in &[u8].
+impl PartialEq for BString {
+    fn eq(&self, other: &Self) -> bool {
+        self.data == other.data
+    }
+
+    fn ne(&self, other: &Self) -> bool {
+        self.data != other.data
+    }
+}
+
+/// Guarantees to be reflexive.
+impl Eq for BString {
+    
+}
+
+/// Simple `Debug` implementation.
+///
+/// Works just like `[u8]::fmt`.
+impl Debug for BString{
+    fn fmt(&self, f: &mut Formatter) -> Result<(), Error> {
+        self.data.fmt(f)
+    }
+}
+
+impl BString {
+    /// Simple constructor from array of bytes.
+    pub fn new(data: &[u8]) -> BString {
+        BString { data: String::from_utf8_lossy(data).into_owned() }
+    }
+}
+
+impl BElement<BString> for BString {
+    /// Decodes `BString` from array of bytes.
+    /// 
+    /// Returns `Ok((position of last used byte in passed array, parsed BString))`
+    /// or `Err` if couldn't parse `BString` correctly.
+    /// # Examples
+    /// 
+    /// `BString` must have following structure: <length>:<data>, where data - sequence
+    /// of bytes with corresponding length.
+    ///
+    /// ```
+    /// use bencode::BElement;
+    /// use bencode::BString;
+    ///
+    /// assert_eq!((4, BString::new("abc".as_bytes())), 
+    ///            BString::decode("3:abc".as_bytes()).ok().expect("invalid"));
+    /// ```
+    fn decode(encoded: &[u8]) -> Result<(usize, BString), &'static str> {
+        let mut i: usize = 0;
+        while i < encoded.len() && encoded[i] as char != ':' {
+            i += 1;
+        }
+        if i == encoded.len() {
+            Err("expected :, but end was found")
+        } else {
+            let length: &[u8] = &encoded[0..i];
+            if let Ok(x) = String::from_utf8_lossy(length).into_owned().parse::<usize>() {
+                let value: &[u8] = &encoded[i + 1..i + x + 1];
+                Ok((i + x, BString::new(value)))
+            } else {
+                Err("expected correct usize number, representing length")
+            }
         }
     }
 }
